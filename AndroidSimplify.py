3ed17a096e30c5743b97d158357f294cbc3880bf
MaksymBaikovets <bmawot@gmail.com>
2019-03-04 11:59:43 +0200

    adding categories

--- AndroidSimplify.py
+++ AndroidSimplify.py
@@ -1,7 +1,7 @@
 from os import system
 import time
 import subprocess
-import platform
+#import platform
 
 program_owner = "* This program was written by Maksym Baikovets."
 disclimer = "* All you done here are under your responsibility."
@@ -24,57 +24,115 @@ def decore(func):
             pass
     return run
 
-def exit():
-    print(input("See you next time! Bye... (Press any key to close the window)"))
-    raise SystemExit
+def go_back():
+    print("You'll be turned back to main menu.")
+    return 
+    # print(input("See you next time! Bye... (Press any key to close the window)"))
+    # raise SystemExit
 
 def adb_devices():
-    cmd = subprocess.run(["C:\\src\\platform-tools\\adb", "devices"], capture_output = False)
+    cmd = subprocess.run([".\\platform-tools-windows\\adb", "devices"], capture_output = False)
     return cmd
  
 def reboot_bootloader():
-    cmd = subprocess.run(["C:\\src\\platform-tools\\adb", "reboot", "bootloader"], capture_output = False)
+    cmd = subprocess.run([".\\platform-tools-windows\\adb", "reboot", "bootloader"], capture_output = False)
     return cmd
 
 def reboot_recovery():
-    cmd = subprocess.run(["C:\\src\\platform-tools\\adb", "reboot", "recovery"], capture_output = False)
+    cmd = subprocess.run([".\\platform-tools-windows\\adb", "reboot", "recovery"], capture_output = False)
     return cmd
 
 def soft_reboot():
-    cmd = subprocess.run(["C:\\src\\platform-tools\\adb", "reboot"], capture_output = False)
+    cmd = subprocess.run([".\\platform-tools-windows\\adb", "reboot"], capture_output = False)
     return cmd
 
-switcher = {
-        0: exit,
+cmd_switcher = {
+        0: go_back,
         1: adb_devices,
         2: reboot_bootloader,
         3: reboot_recovery,
         4: soft_reboot
-
     }
 
 def operation_select(argument):
     # Get the command from switcher dictionary
-    func = switcher.get(argument)
+    func = cmd_switcher.get(argument)
+    return func()
+
+def exit_program():
+    print(input("See you next time! Bye... (Press any key to close the window)"))
+    raise SystemExit
+
+@decore
+def basic_functions():
+    while True:
+        try:
+            print("Please, choose the command to execute (number): ")
+            print("1 - Check connected devices")
+            print("2 - Reboot to bootloader")
+            print("3 - Reboot to recovery")
+            print("4 - Soft reboot device")
+            print("0 - Go back to categories")
+            print("".join("-" for i in range(80)))
+
+            print("Type the command, please:", end = " ")
+            user_input = int(input())
+            
+            output = operation_select(user_input)          
+            print(output, " ", "(Press enter to continue)")
+            input()
+               
+        except ValueError:
+            print("Not integer received! Please, try again!")
+            time.sleep(2)
+        except TypeError:
+            print("Out of available options! Please, try again!")
+            time.sleep(2)
+        break
+    
+@decore
+def recovery_firmware():
+    pass
+
+@decore
+def flashing_rom():
+    pass
+
+@decore
+def modifications_install():
+    pass
+
+category_switcher = {
+    0: exit_program,
+    1: basic_functions,
+    2: recovery_firmware,
+    3: flashing_rom,
+    4: modifications_install
+}
+
+def category_select(argument):
+    # Get the command from switcher dictionary
+    func = category_switcher.get(argument)
     return func()
 
 @decore
 def main_loop():
     while True:
         try:
-            print("Targeted OS:", platform.system())
-            print("Please, choose the command to execute (number): ")
-            print("0 - exit program")
-            print("1 - check connected devices")
-            print("2 - reboot to bootloader")
-            print("3 - reboot to recovery")
-            print("4 - soft reboot device")
-            print("Type the command, please:", end = " ")
-            user_input = int(input())
+            # For the crossplatform support feature:
+            # print("Targeted OS:", platform.system())
+            print("Please, choose the category of CMDs (number): ")
+            print("1 - Basics (adb devices, rebooting)")
+            print("2 - Working with recovery & FW (fastboot, fw version, update recovery / fw)")
+            print("3 - Flashing ROM to device (flash ROM and GAPPs if needed)")
+            print("4 - Install modifications (such as Magisk, launchers, icons, etc.)")
+            print("0 - Exit program")
+            print("".join("-" for i in range(80)))
+            
+            print("Choose the category, please:", end = " ")
+            selector = int(input())
+            category_select(selector)
 
-            output = operation_select(user_input)
-            print(output, " ", "(Press enter to continue)")
-            input()
         except ValueError:
             print("Not integer received! Please, try again!")
             time.sleep(2)
